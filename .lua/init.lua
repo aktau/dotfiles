4cb6053cd4233c6b486fad15c86c09c83a77e308
Nicolas Hillegeer <nicolas@hillegeer.com>
2016-05-10 11:14:48 +0200

    lua: get rid of le(1)'s dependency on Microlight
    Since luarocks still doesn't work properly on my OSX installs
    (keplerproject/luarocks#428).
    
    Functionality wise it should be more or less identical, except you can't do
    the following anymore:
    
      le '"%d > %d?", 1, 2'
    
    Instead, do:
    
      le '"%d > %d" % {1,2}'
    
    Which is only slightly more cumbersome.
    
    Tables are still printed out in human-readable format.

--- .lua/init.lua
+++ .lua/init.lua
@@ -18,8 +18,161 @@ has_ffi, ffi     = pcall(require, 'ffi')
 -- Detect luajit.
 has_lj           = type(jit) == 'table'
 
--- Basic lua version of ml.compose(io.write, string.format).
-printf = function(s,...) return io.write(s:format(...)) end
+-- Override tostring so that it also handles tables. We can't just alter the
+-- table metatable to have a __tostring method because each new table gets a
+-- clean metatable. Implementation from
+-- http://lua-users.org/lists/lua-l/2006-11/msg00079.html. Thanks Aaron
+-- Brown!
+do
+  -- Save the original implementation in _tostring.
+  local _tostring = tostring
+
+  -- Characters that have non-numeric backslash-escaped versions:
+  local BsChars = {
+    ["\a"] = "\\a",
+    ["\b"] = "\\b",
+    ["\f"] = "\\f",
+    ["\n"] = "\\n",
+    ["\r"] = "\\r",
+    ["\t"] = "\\t",
+    ["\v"] = "\\v",
+    ["\""] = "\\\"",
+    ["\\"] = "\\\\"
+  }
+
+  -- Is Str an "escapeable" character (a non-printing character other than
+  -- space, a backslash, or a double quote)?
+  local function IsEscapeable(Char)
+    return string.find(Char, "[^%w%p]") -- Non-alphanumeric, non-punct.
+      and Char ~= " "                   -- Don't count spaces.
+      or string.find(Char, '[\\"]')     -- A backslash or quote.
+  end
+
+  -- Converts an "escapeable" character (a non-printing character,
+  -- backslash, or double quote) to its backslash-escaped version; the
+  -- second argument is used so that numeric character codes can have one or
+  -- two digits unless three are necessary, which means that the returned
+  -- value may represent both the character in question and the digit after
+  -- it:
+  local function EscapeableToEscaped(Char, FollowingDigit)
+    if IsEscapeable(Char) then
+      local Format = FollowingDigit == ""
+        and "\\%d"
+        or "\\%03d" .. FollowingDigit
+      return BsChars[Char]
+        or string.format(Format, string.byte(Char))
+    else
+      return Char .. FollowingDigit
+    end
+  end
+
+  -- Quotes a string in a Lua- and human-readable way.  (This is a
+  -- replacement for string.format's %q placeholder, whose result isn't
+  -- always human readable.)
+  local function StrToStr(Str)
+    return '"' .. string.gsub(Str, "(.)(%d?)", EscapeableToEscaped) .. '"'
+  end
+
+  -- Lua keywords:
+  local Keywords = {["and"] = true, ["break"] = true, ["do"] = true,
+    ["else"] = true, ["elseif"] = true, ["end"] = true, ["false"] = true,
+    ["for"] = true, ["function"] = true, ["if"] = true, ["in"] = true,
+    ["local"] = true, ["nil"] = true, ["not"] = true, ["or"] = true,
+    ["repeat"] = true, ["return"] = true, ["then"] = true,
+    ["true"] = true, ["until"] = true, ["while"] = true}
+
+  -- Is Str an identifier?
+  local function IsIdent(Str)
+    return not Keywords[Str] and string.find(Str, "^[%a_][%w_]*$")
+  end
+
+  -- Converts a non-table to a Lua- and human-readable string.
+  local function ScalarToStr(Val)
+    local Ret
+    local Type = type(Val)
+    if Type == "string" then
+      Ret = StrToStr(Val)
+    elseif Type == "function" or Type == "userdata" or Type == "thread" then
+      -- Punt:
+      Ret = "<" .. _tostring(Val) .. ">"
+    else
+      Ret = _tostring(Val)
+    end -- if
+    return Ret
+  end
+
+  -- Converts a table to a Lua- and human-readable string.
+  local function TblToStr(Tbl, Seen)
+    Seen = Seen or {}
+    local Ret = {}
+    if not Seen[Tbl] then
+      Seen[Tbl] = true
+      local LastArrayKey = 0
+      for Key, Val in pairs(Tbl) do
+        if type(Key) == "table" then
+          Key = "[" .. TblToStr(Key, Seen) .. "]"
+        elseif not IsIdent(Key) then
+          -- If the key is an identifier (and not a regular numeric index),
+          -- we have to use the { [...] = ... } table syntax.
+          if type(Key) == "number" and Key == LastArrayKey + 1 then
+            -- Don't mess with Key if it's an array key.
+            LastArrayKey = Key
+          else
+            Key = "[" .. ScalarToStr(Key) .. "]"
+          end
+        end
+        if type(Val) == "table" then
+          Val = TblToStr(Val, Seen)
+        else
+          Val = ScalarToStr(Val)
+        end
+        Ret[#Ret + 1] = (type(Key) == "string"
+          and (Key .. " = ") -- Explicit key.
+          or "")             -- Implicit array key.
+          .. Val
+      end
+      Ret = "{" .. table.concat(Ret, ",") .. "}"
+    else
+      Ret = "<cycle to " .. _tostring(Tbl) .. ">"
+    end
+    return Ret
+  end
+
+  -- Override global tostring. It handles tables specially (except those
+  -- that have a custom __tostring method), by printing their structure
+  -- instead of <table: 0x...>.
+  function tostring(t)
+    if type(t) == 'table' and not
+      (getmetatable(t) and getmetatable(t).__tostring) then
+      return TblToStr(t)
+    end
+    return _tostring(t)
+  end
+end
+
+-- Basic lua version of ml.compose(io.write, string.format). Alternatively,
+-- you can use io.write(aString % {values}).
+function printf(s, ...)
+  return io.write(s:format(...))
+end
+
+-- Add the % operator to string types. It is used to format strings, like in
+-- Python.
+--
+-- EXAMPLE
+--
+--   print("%d is an integer" % 5)
+getmetatable("").__mod = function(a, b)
+  if not b then
+    return a
+  end
+
+  if type(b) == "table" then
+    return string.format(a, unpack(b))
+  end
+
+  return string.format(a, b)
+end
 
 -- Benchmarking support.
 if has_syscall and has_lj and jit.os == "Linux" then
@@ -117,12 +270,16 @@ function isin(val, ...)
   return false
 end
 
-function round(n)
-  return math.floor(n + 0.5)
+if math.round == nil then
+  function math.round(n)
+    return math.floor(n + 0.5)
+  end
 end
 
-function log2(n)
-  return math.log(n) / math.log(2)
+if math.log2 == nil then
+  function math.log2(n)
+    return math.log(n) / math.log(2)
+  end
 end
 
 -- Make all math functions part of the global namespace, reduces typing.
