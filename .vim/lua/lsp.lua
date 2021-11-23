4ffdb07ff5d261c53a6add20180ee5697096ee7c
Nicolas Hillegeer <nicolas@hillegeer.com>
2021-11-23 15:46:49 -0800

    vim/lsp: configure sumneko_lua better

--- .vim/lua/lsp.lua
+++ .vim/lua/lsp.lua
@@ -221,13 +221,17 @@ local servers = {
   ['sumneko_lua'] = {
     settings = {
       Lua = {
+        runtime = {
+          version = 'LuaJIT',
+          path = vim.split(package.path, ';'),
+        },
         diagnostics = {
           globals = { 'vim' },
         },
-        runtime = { version = 'LuaJIT' },
         workspace = {
           library = {
             [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
           }
         }
       }
