b17218d1427f9f1f9f791718135d854b0ce50f3f
Nicolas Hillegeer <nicolas@hillegeer.com>
2022-06-22 03:52:23 -0700

    vim: deal with incompatible hrsh7th/nvim-cmp changes

--- .vim/lua/lsp.lua
+++ .vim/lua/lsp.lua
@@ -24,15 +24,26 @@ if has_cmp and has_cmp_nvim_lsp then
         vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
       end,
     },
-    mapping = {
+    mapping = cmp.mapping.preset.insert({
       ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.close(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    },
-    sources = {
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
       { name = 'nvim_lsp' },
+    }, {
       { name = 'buffer' },
-    }
+    })
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
   })
 end
 
