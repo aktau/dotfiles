993ddf0993f203cc47b121dbad94eaa052cee295
Nicolas Hillegeer <nicolas@hillegeer.com>
2025-03-25 02:38:02 -0700

    vim/lsp: assert that client is set
    Gets rid of some annoying LuaLS warnings.

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -158,8 +158,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
   group = lsp_augroup,
   callback = function(args)
     local bufnr = args.buf
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
     -- Disable semantic highlighting. It has the Christmas tree effect [1] and
     -- there's some bug triggered by an internal LSP I haven't yet tracked down
     -- [2].
